7c1ccaaab07e5e988439c38488bfecf02d927a8c
lucas6561 <108161041+lucas6561@users.noreply.github.com>
2025-08-31 20:09:23 -0400

    refactor: modularize conjugate main lift selection

--- src/workout_builder/conjugate.rs
+++ src/workout_builder/conjugate.rs
@@ -1,20 +1,28 @@
 use chrono::Weekday;
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::{seq::SliceRandom, thread_rng};
 
 use crate::database::{Database, DbResult};
-use crate::models::{Lift, LiftRegion, LiftType};
+use crate::models::{Lift, LiftType};
 
-use super::{CircuitLift, SingleLift, Workout, WorkoutBuilder, WorkoutLift, WorkoutWeek};
+use super::{SingleLift, Workout, WorkoutBuilder, WorkoutLift, WorkoutWeek};
 
 /// Workout builder implementing a basic conjugate approach.
 pub struct ConjugateWorkoutBuilder;
 
-impl WorkoutBuilder for ConjugateWorkoutBuilder {
-    fn get_wave(&self, num_weeks: usize, db: &dyn Database) -> DbResult<Vec<WorkoutWeek>> {
-        let mut rng = thread_rng();
+/// Holds shuffled pools of main lifts and iteration state.
+struct MainLiftPools {
+    squats: Vec<Lift>,
+    deadlifts: Vec<Lift>,
+    benches: Vec<Lift>,
+    overheads: Vec<Lift>,
+    squat_idx: usize,
+    dead_idx: usize,
+    bench_idx: usize,
+    ohp_idx: usize,
+}
 
-        // Fetch main lift variations
+impl MainLiftPools {
+    fn new(num_weeks: usize, db: &dyn Database) -> DbResult<Self> {
         let mut squats = db.lifts_by_type(LiftType::Squat)?;
         let mut deadlifts = db.lifts_by_type(LiftType::Deadlift)?;
         let mut benches = db.lifts_by_type(LiftType::BenchPress)?;
@@ -38,98 +46,78 @@ impl WorkoutBuilder for ConjugateWorkoutBuilder {
             return Err("not enough overhead press lifts available".into());
         }
 
+        let mut rng = thread_rng();
         squats.shuffle(&mut rng);
         deadlifts.shuffle(&mut rng);
         benches.shuffle(&mut rng);
         overheads.shuffle(&mut rng);
 
-        // Accessory circuits for upper body days
-        let accessories = db.lifts_by_region_and_type(LiftRegion::UPPER, LiftType::Accessory)?;
-        if accessories.len() < 3 {
-            return Err("not enough upper accessory lifts available".into());
+        Ok(Self {
+            squats,
+            deadlifts,
+            benches,
+            overheads,
+            squat_idx: 0,
+            dead_idx: 0,
+            bench_idx: 0,
+            ohp_idx: 0,
+        })
+    }
+
+    fn next_lower(&mut self, week_idx: usize) -> Lift {
+        if week_idx % 2 == 0 {
+            let lift = self.squats[self.squat_idx].clone();
+            self.squat_idx += 1;
+            lift
+        } else {
+            let lift = self.deadlifts[self.dead_idx].clone();
+            self.dead_idx += 1;
+            lift
         }
+    }
 
-        let circuit = |rng: &mut _, acc: &[Lift]| -> CircuitLift {
-            let picks: Vec<SingleLift> = acc
-                .choose_multiple(rng, 3)
-                .cloned()
-                .map(|lift| SingleLift {
-                    lift,
-                    rep_count: None,
-                    time_sec: None,
-                    distance_m: None,
-                })
-                .collect();
-            CircuitLift {
-                circuit_lifts: picks,
-                rest_time_sec: 60,
-            }
-        };
+    fn next_upper(&mut self, week_idx: usize) -> Lift {
+        if week_idx % 2 == 0 {
+            let lift = self.benches[self.bench_idx].clone();
+            self.bench_idx += 1;
+            lift
+        } else {
+            let lift = self.overheads[self.ohp_idx].clone();
+            self.ohp_idx += 1;
+            lift
+        }
+    }
+}
 
+impl ConjugateWorkoutBuilder {
+    fn single(lift: Lift) -> WorkoutLift {
+        WorkoutLift::Single(SingleLift {
+            lift,
+            rep_count: None,
+            time_sec: None,
+            distance_m: None,
+        })
+    }
+
+    fn build_week(i: usize, pools: &mut MainLiftPools) -> WorkoutWeek {
+        let mut week = WorkoutWeek::new();
+
+        let lower = pools.next_lower(i);
+        week.insert(Weekday::Mon, Workout { lifts: vec![Self::single(lower)] });
+
+        let upper = pools.next_upper(i);
+        week.insert(Weekday::Tue, Workout { lifts: vec![Self::single(upper)] });
+
+        week
+    }
+}
+
+impl WorkoutBuilder for ConjugateWorkoutBuilder {
+    fn get_wave(&self, num_weeks: usize, db: &dyn Database) -> DbResult<Vec<WorkoutWeek>> {
+        let mut pools = MainLiftPools::new(num_weeks, db)?;
         let mut weeks = Vec::with_capacity(num_weeks);
-        let mut squat_idx = 0;
-        let mut dead_idx = 0;
-        let mut bench_idx = 0;
-        let mut ohp_idx = 0;
-
         for i in 0..num_weeks {
-            let mut week = WorkoutWeek::new();
-
-            // Monday alternates squat and deadlift variations
-            let lower_lift = if i % 2 == 0 {
-                let lift = squats[squat_idx].clone();
-                squat_idx += 1;
-                lift
-            } else {
-                let lift = deadlifts[dead_idx].clone();
-                dead_idx += 1;
-                lift
-            };
-            let mon_lift = SingleLift {
-                lift: lower_lift,
-                rep_count: None,
-                time_sec: None,
-                distance_m: None,
-            };
-            week.insert(
-                Weekday::Mon,
-                Workout {
-                    lifts: vec![WorkoutLift::Single(mon_lift)],
-                },
-            );
-
-            // Tuesday alternates bench and overhead press variations
-            let upper_lift = if i % 2 == 0 {
-                let lift = benches[bench_idx].clone();
-                bench_idx += 1;
-                lift
-            } else {
-                let lift = overheads[ohp_idx].clone();
-                ohp_idx += 1;
-                lift
-            };
-            let tue_main = SingleLift {
-                lift: upper_lift,
-                rep_count: None,
-                time_sec: None,
-                distance_m: None,
-            };
-            let mut tue_lifts = vec![WorkoutLift::Single(tue_main)];
-
-            let tue_circuit = circuit(&mut rng, &accessories);
-            tue_lifts.push(WorkoutLift::Circuit(tue_circuit));
-            week.insert(Weekday::Tue, Workout { lifts: tue_lifts });
-
-            // Thursday accessory circuit
-            let thu_circuit = circuit(&mut rng, &accessories);
-            week.insert(
-                Weekday::Thu,
-                Workout {
-                    lifts: vec![WorkoutLift::Circuit(thu_circuit)],
-                },
-            );
-
-            weeks.push(week);
+            weeks.push(Self::build_week(i, &mut pools));
         }
         Ok(weeks)
     }
--- tests/workout_builder.rs
+++ tests/workout_builder.rs
@@ -16,71 +16,61 @@ use sqlite_db::SqliteDb;
 use workout_builder::{ConjugateWorkoutBuilder, WorkoutBuilder, WorkoutLift};
 
 #[test]
-fn builds_wave_with_expected_structure() {
+fn alternates_main_lifts_across_weeks() {
     let db = SqliteDb::new(":memory:").expect("db open");
+
     // add squat and deadlift lifts
     db.add_lift("Back Squat", LiftRegion::LOWER, LiftType::Squat, &[], "")
         .unwrap();
+    db.add_lift("Front Squat", LiftRegion::LOWER, LiftType::Squat, &[], "")
+        .unwrap();
     db.add_lift("Conventional Deadlift", LiftRegion::LOWER, LiftType::Deadlift, &[], "")
         .unwrap();
+    db.add_lift("Sumo Deadlift", LiftRegion::LOWER, LiftType::Deadlift, &[], "")
+        .unwrap();
+
     // add bench and overhead press lifts
     db.add_lift("Bench Press", LiftRegion::UPPER, LiftType::BenchPress, &[], "")
         .unwrap();
+    db.add_lift("Close-Grip Bench Press", LiftRegion::UPPER, LiftType::BenchPress, &[], "")
+        .unwrap();
     db.add_lift("Overhead Press", LiftRegion::UPPER, LiftType::OverheadPress, &[], "")
         .unwrap();
-    // add upper accessory lifts
-    db.add_lift("Curl", LiftRegion::UPPER, LiftType::Accessory, &[], "")
-        .unwrap();
-    db.add_lift("Row", LiftRegion::UPPER, LiftType::Accessory, &[], "")
-        .unwrap();
-    db.add_lift("Fly", LiftRegion::UPPER, LiftType::Accessory, &[], "")
+    db.add_lift("Push Press", LiftRegion::UPPER, LiftType::OverheadPress, &[], "")
         .unwrap();
 
     let builder = ConjugateWorkoutBuilder;
-    let wave = builder.get_wave(2, &db).unwrap();
-    assert_eq!(wave.len(), 2);
+    let wave = builder.get_wave(4, &db).unwrap();
+    assert_eq!(wave.len(), 4);
 
-    // Week 1 should be squat and bench
-    let week1 = &wave[0];
-    let mon = week1.get(&Weekday::Mon).expect("monday");
-    match &mon.lifts[0] {
-        WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(LiftType::Squat)),
-        _ => panic!("expected single"),
-    }
-    let tue = week1.get(&Weekday::Tue).expect("tuesday");
-    match &tue.lifts[0] {
-        WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(LiftType::BenchPress)),
-        _ => panic!("expected single"),
-    }
-    match &tue.lifts[1] {
-        WorkoutLift::Circuit(c) => assert_eq!(c.circuit_lifts.len(), 3),
-        _ => panic!("expected circuit"),
-    }
-    let thu = week1.get(&Weekday::Thu).expect("thursday");
-    match &thu.lifts[0] {
-        WorkoutLift::Circuit(c) => assert_eq!(c.circuit_lifts.len(), 3),
-        _ => panic!("expected circuit"),
-    }
+    let expected_lower = [
+        LiftType::Squat,
+        LiftType::Deadlift,
+        LiftType::Squat,
+        LiftType::Deadlift,
+    ];
+    let expected_upper = [
+        LiftType::BenchPress,
+        LiftType::OverheadPress,
+        LiftType::BenchPress,
+        LiftType::OverheadPress,
+    ];
 
-    // Week 2 should be deadlift and overhead press
-    let week2 = &wave[1];
-    let mon = week2.get(&Weekday::Mon).expect("monday");
-    match &mon.lifts[0] {
-        WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(LiftType::Deadlift)),
-        _ => panic!("expected single"),
-    }
-    let tue = week2.get(&Weekday::Tue).expect("tuesday");
-    match &tue.lifts[0] {
-        WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(LiftType::OverheadPress)),
-        _ => panic!("expected single"),
-    }
-    match &tue.lifts[1] {
-        WorkoutLift::Circuit(c) => assert_eq!(c.circuit_lifts.len(), 3),
-        _ => panic!("expected circuit"),
-    }
-    let thu = week2.get(&Weekday::Thu).expect("thursday");
-    match &thu.lifts[0] {
-        WorkoutLift::Circuit(c) => assert_eq!(c.circuit_lifts.len(), 3),
-        _ => panic!("expected circuit"),
+    for (i, week) in wave.iter().enumerate() {
+        let mon = week.get(&Weekday::Mon).expect("monday");
+        match &mon.lifts[0] {
+            WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(expected_lower[i])),
+            _ => panic!("expected single"),
+        }
+        assert_eq!(mon.lifts.len(), 1);
+
+        let tue = week.get(&Weekday::Tue).expect("tuesday");
+        match &tue.lifts[0] {
+            WorkoutLift::Single(s) => assert_eq!(s.lift.main, Some(expected_upper[i])),
+            _ => panic!("expected single"),
+        }
+        assert_eq!(tue.lifts.len(), 1);
+
+        assert!(week.get(&Weekday::Thu).is_none());
     }
 }
