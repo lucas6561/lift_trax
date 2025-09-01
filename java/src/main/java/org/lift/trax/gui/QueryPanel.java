f9d549d739815fe2c0aab4cc5b32ce2604e23331
lucas6561 <108161041+lucas6561@users.noreply.github.com>
2025-09-01 10:39:07 -0400

    Add Swing GUI package with basic tabs

---
+++ java/src/main/java/org/lift/trax/gui/QueryPanel.java
@@ -0,0 +1,9 @@
+package org.lift.trax.gui;
+
+import javax.swing.*;
+
+public class QueryPanel extends JPanel {
+    public QueryPanel() {
+        add(new JLabel("Query not yet implemented"));
+    }
+}
