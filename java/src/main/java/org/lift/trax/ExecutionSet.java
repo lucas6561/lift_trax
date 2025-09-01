c8e8d0d0a165e1677c11b9f2293edce8b73dc0fb
lucas6561 <108161041+lucas6561@users.noreply.github.com>
2025-09-01 10:12:32 -0400

    Handle metric field in ExecutionSet

--- java/src/main/java/org/lift/trax/ExecutionSet.java
+++ java/src/main/java/org/lift/trax/ExecutionSet.java
@@ -1,5 +1,11 @@
 package org.lift.trax;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExecutionSet {
     public int reps;
     public double weight;
@@ -11,5 +17,14 @@ public class ExecutionSet {
         this.reps = reps;
         this.weight = weight;
         this.rpe = rpe;
+    }
+
+    @JsonProperty("metric")
+    private void unpackMetric(Map<String, Integer> metric) {
+        if (metric == null) return;
+        if (metric.containsKey("Reps")) {
+            this.reps = metric.get("Reps");
+        }
+        // TimeSecs and DistanceFeet are ignored since Java layer only tracks reps currently
     }
 }
