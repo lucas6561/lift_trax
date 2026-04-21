84990fa9b0cb1a3ae9a01747370da693ad727eb5
lucas6561 <108161041+lucas6561@users.noreply.github.com>
2026-04-21 18:34:45 -0400

    Add deterministic random stack tests in Java and Rust

--- rust/tests/random_stack.rs
+++ rust/tests/random_stack.rs
@@ -2,6 +2,7 @@
 mod random_stack;
 
 use random_stack::RandomStack;
+use random_stack::RandomMode;
 
 #[test]
 fn cycles_without_repeating_immediately() {
@@ -22,4 +23,16 @@ fn handles_single_item() {
     let mut stack = RandomStack::new(vec![42]);
     assert_eq!(stack.pop(), Some(42));
     assert_eq!(stack.pop(), Some(42));
+}
+
+#[test]
+fn deterministic_mode_produces_repeatable_sequence() {
+    let mut a = RandomStack::with_mode(vec![1, 2, 3, 4], RandomMode::Deterministic);
+    let mut b = RandomStack::with_mode(vec![1, 2, 3, 4], RandomMode::Deterministic);
+
+    let seq_a: Vec<_> = (0..8).map(|_| a.pop().expect("value")).collect();
+    let seq_b: Vec<_> = (0..8).map(|_| b.pop().expect("value")).collect();
+
+    assert_eq!(seq_a, seq_b);
+    assert_eq!(seq_a, vec![4, 3, 2, 1, 4, 3, 2, 1]);
 }
